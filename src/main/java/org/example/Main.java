5c7cdab7b2d828f61cbe19921ac9b589930ea71d
Francisco Duque de Morais Amaro <f.duquedemoraisamaro@student.tudelft.nl>
2023-04-12 17:26:17 +0200

    Teste 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("teste 2");
          for(int i = 0; i<10 ;i++){
              System.out.println(i);
+             System.out.println("teste 2");
          }
     }
 }
